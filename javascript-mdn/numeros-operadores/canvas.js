51b081b7104d4c0f1f94cba4c1e75449e4bd64f8
Luiz Gustavo <112131771+luizguga@users.noreply.github.com>
2023-01-25 17:53:47 -0300

    Criei numeros-operadores

---
+++ javascript-mdn/numeros-operadores/canvas.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let x = 50; let y = 50;
+x = 250;
+y = 100;
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10,10,x,y);
