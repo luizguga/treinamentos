54233f47cb280615a18417515ba95824424bde23
Luiz Gustavo <112131771+luizguga@users.noreply.github.com>
2023-02-09 16:46:50 -0300

    Avaliação finalizada

--- javascript-mdn/projeto01/main.js
+++ javascript-mdn/projeto01/main.js
@@ -1,7 +1,37 @@
+const img = document.querySelector('.displayed-img')
 const thumbBar = document.querySelector('.thumb-bar')
+const btn = document.querySelector('button')
+const div = document.querySelector('.overlay')
 
-for(let i=1;i<6;i++){
-    let thumb = document.createElement('img')
-    thumb.setAttribute('src', 'images/pic'+i+'.jpg')
-    thumbBar.appendChild(thumb)
+const images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
+const alts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Rock that looks like a wave',
+    'pic3.jpg': 'Purple and white pansies',
+    'pic4.jpg': 'Section of wall from a pharoah\'s tomb',
+    'pic5.jpg': 'Large moth on a leaf'
 }
+
+for(const image of images){
+    let newImage = document.createElement('img')
+    newImage.setAttribute('src', `images/${image}`)
+    newImage.setAttribute('alt', alts[image])
+    thumbBar.appendChild(newImage)
+    newImage.addEventListener('click',(e)=>{
+        img.src = e.target.src
+        img.alt = e.target.alt
+    })
+}
+
+btn.addEventListener('click', function (){
+    let btnClass = btn.getAttribute('class')
+    if(btnClass === 'dark'){
+        btn.setAttribute('class', 'lighten')
+        btn.textContent = 'Lighten'
+        div.style.backgroundColor = 'rgb(0,0,0,.3)'
+    }else{
+        btn.setAttribute('class', 'dark')
+        btn.textContent = 'Dark'
+        div.style.backgroundColor = 'transparent'
+    }
+})
